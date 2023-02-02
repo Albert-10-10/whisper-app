8a77b7daacabd2eedb831c6f999e5f966f8e8efe
Roy Albert Belen <belenroyalbert@gmail.com>
2023-02-02 19:58:31 +0800

    Replaced MD5 with bcrypt for salting and hashing.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,13 +23,7 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-//encrypt
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
-
 const User = mongoose.model("user", userSchema);
-
-
-
 
 app.get("/", (req,res)=>{
     res.render("Home");
@@ -41,14 +36,16 @@ app.route("/login")
     .post((req,res)=>{
         const username = req.body.username;
         const password = req.body.password;
-        User.findOne({email: username}, (err, foundUser)=>{ //find username in db
+        User.findOne({email: username}, (err, foundUser)=>{
             if(err){
                 console.log(err);
             } else {
                 if(foundUser){
-                    if(foundUser.password === password){ // find matching pass if username found
-                        res.render("secrets");
-                    }
+                    bcrypt.compare(password, foundUser.password, (err, result)=> {
+                        if (result === true){
+                            res.render("secrets");
+                        }
+                    });
                 }
             }
         })
@@ -59,17 +56,21 @@ app.route("/register")
         res.render("register");
     })
     .post((req,res)=>{
-        const newUser = new User({
-            email: req.body.username,
-            password : req.body.password
+        bcrypt.hash(req.body.password, saltRounds, (err, hash)=> {
+            const newUser = new User({
+                email: req.body.username,
+                password : hash
+            });
+            newUser.save((err)=>{
+                if(err){
+                    console.log(err);
+                } else {
+                    res.render("secrets");
+                }
+            })
         });
-        newUser.save((err)=>{
-            if(err){
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        })
+
+        
     })
 
 app.listen(3000, ()=>{
